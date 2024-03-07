6b15492834bf9129e777aed0ba95bff4098bde71
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-03-07 23:26:02 +0530

    ui 50

--- lib/widget/grocery_list_widget.dart
+++ lib/widget/grocery_list_widget.dart
@@ -27,10 +27,63 @@ class _GroceryListWidgetState extends State<GroceryListWidget> {
       _groceryItem.add(newItem);
     });
   }
-  
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItem.remove(item);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text(
+        'NO Item',
+      ),
+    );
+
+    if (_groceryItem.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItem.length,
+        itemBuilder: (context, index) => Dismissible(
+          direction: DismissDirection.endToStart,
+          background: Container(
+            color: Colors.red,
+            alignment: Alignment.centerRight,
+            margin: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 15,
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.delete_forever,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          onDismissed: (direction) {
+            _removeItem(
+              _groceryItem[index],
+            );
+          },
+          key: ValueKey(_groceryItem[index].id),
+          child: ListTile(
+            title: Text(_groceryItem[index].name),
+            leading: Container(
+              height: 24,
+              width: 24,
+              color: _groceryItem[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItem[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -43,20 +96,7 @@ class _GroceryListWidgetState extends State<GroceryListWidget> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItem.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryItem[index].name),
-          leading: Container(
-            height: 24,
-            width: 24,
-            color: _groceryItem[index].category.color,
-          ),
-          trailing: Text(
-            _groceryItem[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: content,
     );
   }
 }
